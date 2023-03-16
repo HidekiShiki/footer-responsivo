b78aeaee5d79859ccd48fd5a07d19b5847ab6b22
Hideki Kishi <jp.hidekifk@gmail.com>
2023-03-16 10:34:43 -0300

    Add files via upload
    é necessário baixar a biblioteca node_modules

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Body from './Components/Body/body';
+import Footer from './Components/Footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
